aa2878889bc50f8be535b0564fe989f30ecf22eb
nhatthang01 <51067848+nhatthang01@users.noreply.github.com>
2019-07-11 21:06:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (!process.env.DISABLE_XORIGIN) {
     var origin = req.headers.origin || '*';
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
          console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header('Access-Control-Allow-Origin', origin);
          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     }
     next();
