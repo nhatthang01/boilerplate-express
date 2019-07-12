f142378b63fc9bb757d2aa05d4510835ba84b3b7
nhatthang01 <51067848+nhatthang01@users.noreply.github.com>
2019-07-12 12:18:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * ***************************************************/
  
 var bGround = require('fcc-express-bground');
-var myApp = require('./myApp');
+var myApp = require('./hello-console');
 var express = require('express');
 var app = express();
 
